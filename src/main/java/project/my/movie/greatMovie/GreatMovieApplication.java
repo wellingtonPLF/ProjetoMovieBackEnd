7218bb1044e13f3184b5ba4baab2c7ec1c36aba7
WellingtonPLF <wellplf@gmail.com>
2022-05-15 19:34:15 -0300

    First Commit

---
+++ src/main/java/project/my/movie/greatMovie/GreatMovieApplication.java
@@ -0,0 +1,13 @@
+package project.my.movie.greatMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/my/movie/greatMovie/GreatMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package project.my.movie.greatMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
